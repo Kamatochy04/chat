e3667e768d93a173852f9fab1e040d4fe2d30b30
Andrey Valiuk <andrey.valyuk.04@mail.ru>
2025-03-17 15:26:17 +0300

    add start settings

---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+import '@/styles/index.css';
+import '@/styles/fonts.css';
+
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+createRoot(document.getElementById('root')!).render(<App />);
